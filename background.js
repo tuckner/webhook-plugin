f7b89b021250edeca9b67d723a07f75bfe6936e9
tuckner <tuckner329@gmail.com>
2024-03-18 15:45:20 -0500

    added text selection to plugin

--- background.js
+++ background.js
@@ -2,33 +2,35 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
     "id": "sendToWebhook",
     "title": "Send page to webhook",
-    "contexts": ["page"]
+    "contexts": ["page", "selection"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
   if (info.menuItemId == "sendToWebhook") {
-    // First, get the 'sendBody' setting value
-    chrome.storage.sync.get(['webhookUrl', 'sendBody'], function(data) {
+    // Get the 'webhookUrl' from settings
+    chrome.storage.sync.get(['webhookUrl'], function(data) {
       if (data.webhookUrl) {
-        let body = { url: info.pageUrl };
-
-        // Check if 'sendBody' is true
-        if (data.sendBody) {
-          // Execute the content script to get the page's HTML content
+        // Determine whether to send selected text or whole page
+        if (info.selectionText) {
+          // User has selected text; send the selection
+          sendWebhookRequest(data.webhookUrl, {
+            url: info.pageUrl,
+            contents: info.selectionText
+          });
+        } else {
+          // No text selected; send the whole page content
           chrome.scripting.executeScript({
-            target: { tabId: tab.id },
+            target: {tabId: tab.id},
             function: getPageContent
           }, (injectionResults) => {
             if (injectionResults && injectionResults[0]) {
-              body.contents = injectionResults[0].result;
+              sendWebhookRequest(data.webhookUrl, {
+                url: info.pageUrl,
+                contents: injectionResults[0].result
+              });
             }
-            // Send the body to the webhook
-            sendWebhookRequest(data.webhookUrl, body);
           });
-        } else {
-          // If 'sendBody' is not true, send only the URL
-          sendWebhookRequest(data.webhookUrl, body);
         }
       } else {
         console.error('Webhook URL is not defined.');
@@ -37,12 +39,12 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
   }
 });
 
-// This function will be injected into the page to retrieve the page content
+// Function to get the page content
 function getPageContent() {
   return document.documentElement.outerHTML;
 }
 
-// This function sends the data to the webhook
+// Function to send the data to the webhook
 function sendWebhookRequest(webhookUrl, body) {
   fetch(webhookUrl, {
     method: 'POST',
